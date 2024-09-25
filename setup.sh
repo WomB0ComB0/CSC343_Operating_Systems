e4409af97d8e41ab7a7b577c44208e842384e51a
Mike Odnis <airwolf635@gmail.com>
2024-09-25 09:20:18 -0400

    chore: setup 2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 for i in {1..20}; do
     mkdir week-$i
+    touch ./week-$i/.gitkeep
     touch ./week-$i/README.md
+    mkdir week-$i/classwork
+    touch ./week-$i/classwork/.gitkeep
+    mkdir week-$i/homework
+    touch ./week-$i/homework/.gitkeep
 done
