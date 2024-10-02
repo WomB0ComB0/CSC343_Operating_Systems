8874afca4b9068e3cbc345e429b27efb71e8b9ce
Mike Odnis <airwolf635@gmail.com>
2024-10-02 11:00:26 -0400

    chore: week 4 work c

--- week-4/classwork/worka.cpp
+++ week-4/classwork/worka.cpp
@@ -11,15 +11,15 @@ int main() {
     pthread_t thread1;
     pthread_t thread2;
     pthread_create(&thread1, NULL, thread_function, NULL);
-     printf("Hello from the main!\n");
-     printf("After thread 1 call, the thread id = %d\n", thread1);
-     
+    printf("Hello from the main!\n");
+    printf("After thread 1 call, the thread id = %d\n", thread1);
+
     pthread_create(&thread2, NULL, thread_function, NULL);
-     printf("Hello from the main-1\n");
-     printf("After thread 2 call, the thread id = %d\n", thread2);
-  
+    printf("Hello from the main-1\n");
+    printf("After thread 2 call, the thread id = %d\n", thread2);
+
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-     printf("Hello from the main-2\n");
+    printf("Hello from the main-2\n");
     return 0;
 }
--- week-4/classwork/workb.cpp
+++ week-4/classwork/workb.cpp
@@ -12,16 +12,16 @@ int main() {
     pthread_t thread1;
     pthread_t thread2;
     pthread_create(&thread1, NULL, thread_function, NULL);
-     printf("Hello from the main!\n");
-     printf("After thread 1 call, the thread id = %d\n", thread1);
-     
+    printf("Hello from the main!\n");
+    printf("After thread 1 call, the thread id = %d\n", thread1);
+
     pthread_create(&thread2, NULL, thread_function, NULL);
-     printf("Hello from the main-1\n");
-     printf("After thread 2 call, the thread id = %d\n", thread2);
-  
+    printf("Hello from the main-1\n");
+    printf("After thread 2 call, the thread id = %d\n", thread2);
+
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
-     printf("Hello after the join\n");
+    printf("Hello after the join\n");
     return 0;
 }
 
